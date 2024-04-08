6ea0704e3267da85354c177f6a4d5c20ee0431b6
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2024-04-08 12:50:11 +0200

    inital commit

---
+++ cookie.js
@@ -0,0 +1,13 @@
+var score = 0;
+
+var cookie = document.getElementById("cookieBtn");
+
+function clicked() {
+    alert("GAY!");
+    score++;
+}
+
+
+
+
+cookie.addEventListener('click', startGame);
