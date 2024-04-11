e7470088ba1059a97793e9d0960fabe39818e60f
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2024-04-11 13:17:35 +0200

    they spawn more
    yeahyeah

--- cookie.js
+++ cookie.js
@@ -1,14 +1,44 @@
 var score = 0;
 
+var spawnCount = 0;
+
 const cookie = document.getElementById("cookieBtn");
-const scoreCounter = document.getElementById("score")
-console.Log("yes")
+const scoreCounter = document.getElementById("score");
 
 
+function moveCookie(_styleLeft, newCookieId) {
+    setInterval(cookieMove(newCookieId), 1000);
+}
+
+function cookieMove(newCookieId, styleLeft=1) {
+    console.log(newCookieId);
+    console.log(styleLeft);
+    styleLeft += 10;
+    document.getElementById(newCookieId).style.marginLeft = styleLeft + "px";
+}
+
+    
+
+
+function spawn(move) {
+    spawnCount++;
+    var newNode = document.createElement("button");
+    newNode.className = "cookieBtn";
+    newNodeId = "cookie_" + spawnCount;
+    newNode.id = newNodeId;
+    newNode.innerHTML = "COOKIE";
+    document.getElementById("cookies").appendChild(newNode);
+    document.getElementById(newNodeId).style.top = Math.floor(Math.random() * 100) + "%";
+
+    
+    if (move == true) {
+        moveCookie(1, newNodeId);
+    }
+}
 
 function clickedCookie() {
     score++;
     scoreCounter.innerHTML = score;
-    cookie.style.left = Math.floor(Math.random() * 100) + "%"
-    cookie.style.top = Math.floor(Math.random() * 100) + "%"
+    spawn(move=true);
 }
+
