4c5527d05c851912483fbba5ff742c8e1a2e9e29
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2024-04-29 12:39:25 +0200

    cookies go vroom vroom

--- cookie.js
+++ cookie.js
@@ -6,39 +6,49 @@ const cookie = document.getElementById("cookieBtn");
 const scoreCounter = document.getElementById("score");
 
 
-function moveCookie(_styleLeft, newCookieId) {
-    setInterval(cookieMove(newCookieId), 1000);
-}
 
-function cookieMove(newCookieId, styleLeft=1) {
-    console.log(newCookieId);
-    console.log(styleLeft);
-    styleLeft += 10;
-    document.getElementById(newCookieId).style.marginLeft = styleLeft + "px";
-}
+document.documentElement.requestFullscreen();
 
-    
-
-
-function spawn(move) {
-    spawnCount++;
-    var newNode = document.createElement("button");
-    newNode.className = "cookieBtn";
-    newNodeId = "cookie_" + spawnCount;
-    newNode.id = newNodeId;
-    newNode.innerHTML = "COOKIE";
-    document.getElementById("cookies").appendChild(newNode);
-    document.getElementById(newNodeId).style.top = Math.floor(Math.random() * 100) + "%";
-
-    
-    if (move == true) {
-        moveCookie(1, newNodeId);
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
     }
 }
 
-function clickedCookie() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+sleep(1000).then(() => { toggleFullScreen(); }); //toggles fullscreen almost as soon as game starts
+
+let spawnInterval = setInterval(spawn, 2500);
+let FullscreenInterval = setInterval(toggleFullScreen, 10000);
+
+
+function spawn() { //spawns the cookie, then deletes it if it has been alive for too long
+    spawnCount++;
+    let newNode = document.createElement("button")
+    newNode.className = "cookieBtn";
+    newNodeId = "cookie_" + spawnCount;
+    newNode.id = newNodeId;
+    document.getElementById("cookies").appendChild(newNode);
+    document.getElementById(newNodeId).style.top = Math.floor(Math.random() * 100) + "%";
+    newNode.classList = "cookieDefault"
+    newNode.setAttribute("onclick", "clickedCookie(this.id)");
+    img = document.createElement("img")
+    newNode.appendChild(img)
+    img.setAttribute("src", "./images/cookies/chocolate_chip.png")
+    img.classList = "cookieImg"
+    sleep(10000).then(() => { newNode.remove(); });
+}
+
+function clickedCookie(clickedCookieId) { //points + kill on click
     score++;
     scoreCounter.innerHTML = score;
-    spawn(move=true);
+    document.getElementById(clickedCookieId).remove();
 }
+
+
 
