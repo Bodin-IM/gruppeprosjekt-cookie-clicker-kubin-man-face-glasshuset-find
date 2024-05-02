7a98edbbe3608add57829818c069fd7c9c943bc0
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2024-05-02 10:59:04 +0200

    fullscreenPromt()

--- cookie.js
+++ cookie.js
@@ -5,11 +5,11 @@ var spawnCount = 0;
 const cookie = document.getElementById("cookieBtn");
 const scoreCounter = document.getElementById("score");
 
+fullscreenPrompt()
 
-
-document.documentElement.requestFullscreen();
 
 function toggleFullScreen() {
+    document.getElementById("promptBackground").style.display = "none"
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
     }
@@ -19,29 +19,40 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function fullscreenPrompt() {
+    if (!document.fullscreenElement) {
+        document.getElementById("promptBackground").style.display = "flex"
+    } else {
+        document.getElementById("promptBackground").style.display = "none"
+    }
+}
 
 
-sleep(1000).then(() => { toggleFullScreen(); }); //toggles fullscreen almost as soon as game starts
+
 
 let spawnInterval = setInterval(spawn, 2500);
-let FullscreenInterval = setInterval(toggleFullScreen, 10000);
-
+let fullscreenInterval = setInterval(fullscreenPrompt, 10);
 
 function spawn() { //spawns the cookie, then deletes it if it has been alive for too long
-    spawnCount++;
-    let newNode = document.createElement("button")
-    newNode.className = "cookieBtn";
-    newNodeId = "cookie_" + spawnCount;
-    newNode.id = newNodeId;
-    document.getElementById("cookies").appendChild(newNode);
-    document.getElementById(newNodeId).style.top = Math.floor(Math.random() * 100) + "%";
-    newNode.classList = "cookieDefault"
-    newNode.setAttribute("onclick", "clickedCookie(this.id)");
-    img = document.createElement("img")
-    newNode.appendChild(img)
-    img.setAttribute("src", "./images/cookies/chocolate_chip.png")
-    img.classList = "cookieImg"
-    sleep(10000).then(() => { newNode.remove(); });
+    if (document. fullscreenElement) {
+    
+        spawnCount++;
+        let newNode = document.createElement("button")
+        newNode.className = "cookieBtn";
+        newNodeId = "cookie_" + spawnCount;
+        newNode.id = newNodeId;
+        document.getElementById("cookies").appendChild(newNode);
+        document.getElementById(newNodeId).style.top = Math.floor(Math.random() * 50) + "%";
+        newNode.classList = "cookieDefault"
+        newNode.setAttribute("onclick", "clickedCookie(this.id)");
+        img = document.createElement("img")
+        newNode.appendChild(img)
+        img.setAttribute("src", "./images/cookies/chocolate_chip.png")
+        img.classList = "cookieImg"
+        sleep(10000).then(() => { newNode.remove(); });
+    } else {
+        fullscreenPrompt()
+    }
 }
 
 function clickedCookie(clickedCookieId) { //points + kill on click
